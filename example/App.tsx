f872177578eda84ca760dd6af7e4383070acce59
FreakyCoder <kurayogun@gmail.com>
2021-05-19 22:28:38 +0300

    new: Re-written radio button is here

--- example/App.tsx
+++ example/App.tsx
@@ -1,70 +1,30 @@
 import React from "react";
 import {
-  View,
-  StatusBar,
-  ImageBackground,
-  Text,
   SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
 } from "react-native";
-import RadioButton from "react-native-animated-radio-button";
+import RadioButton from "./lib/RadioButton";
 
 const App = () => {
+  const isDarkMode = useColorScheme() === "dark";
+
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <RadioButton
-          size={50}
-          isSelected
-          horizontal={false}
-          innerColor="#e82020"
-          outerColor="#e82020"
-          animation={"bounceIn"}
-          onPress={() => console.log("RadioButton is pressed")}
-        />
-        <View style={{ marginTop: 16 }}>
-          <RadioButton
-            size={35}
-            text="Medium"
-            isSelected={false}
-            isBounceable={false}
-            animation={"bounceIn"}
-            onPress={() => console.log("RadioButton is pressed")}
-          />
-        </View>
-        <View style={{ marginTop: 16 }}>
-          <RadioButton
-            size={25}
-            isSelected
-            text="Mini"
-            innerColor="#9e29d9"
-            outerColor="#6a29d9"
-            animation={"bounceIn"}
-            onPress={() => console.log("RadioButton is pressed")}
-            customImageComponent={
-              <ImageBackground
-                source={require("./assets/party.png")}
-                style={{
-                  height: 50,
-                  width: 50,
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Text>Hello</Text>
-              </ImageBackground>
-            }
-          />
-        </View>
-      </SafeAreaView>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <RadioButton
+        onPress={(isActive: boolean) => console.log("isActive: ", isActive)}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default App;
